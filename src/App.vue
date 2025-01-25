8caf61de98eb958447c0506ac3d095b29ef01dd5
Kamil Akhmedkhanov <=>
2025-01-25 20:24:19 +0300

    Добавил базовые библиотеки

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { useStore } from '@/store.ts'
+
+const store = useStore()
 </script>
 
 <template>
   <main>
+    <h1>{{ store.value }}</h1>
   </main>
 </template>
 
